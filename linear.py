338d84972963f0745b3c97dfab67d546e71ec56c
Jacob Volkman <volkmanj@vm-cs434-1.engr.oregonstate.edu>
2018-04-14 17:12:19 -0700

    added comments to linear regression file

--- linear.py
+++ linear.py
@@ -24,37 +24,57 @@ def cal_weight(X,Y):
 	Y_tr = numpy.transpose(Y)
 	#https://docs.scipy.org/doc/numpy/reference/generated/numpy.dot.html
 	w = numpy.dot(inv(numpy.dot(X_tr,X)),numpy.dot(X_tr,Y))
+
 	return w
 
 def ASE(X,Y,w):
 	temp = 0
 	for a in range (len(Y)):
 		temp += (float((Y[a] - numpy.dot(X[a],w))))**2
-	return temp/len(Y)
+	return temp/len(Y) #this is ASE.
 
 def add_feature(rand_num):
+	
+	#while index is greater than 0
+		#some variable d = random sample from normal distribution
+		#append d to instance of X
+		#index--
+		
+		#however, I have no idea how to implement it in this version of Python
+	
 	#Working on it now
 	return
 
 file_name1 = 'housing_train.txt'
 file_name2 = 'housing_test.txt'
 
-tr_X, tr_Y = open_file(file_name1,1) # for question 1
-ts_X, ts_Y = open_file(file_name2,1)
-w = cal_weight(tr_X,tr_Y)
-print('Weight with dummy:')
-print(numpy.matrix(w)) #question 1
-#print(tr_X)
-print('ASE with dummy:')
-print(ASE(tr_X, tr_Y,w))
-print(ASE(ts_X, ts_Y,w))
+#question 1.1
+tr_X, tr_Y = open_file(file_name1,1) #transposes training data with dummy var
+ts_X, ts_Y = open_file(file_name2,1) #transposes testing data with dummy var
 
-tr_X, tr_Y = open_file(file_name1,0) # w/o dummy
-ts_X, ts_Y = open_file(file_name2,0)
-w = cal_weight(tr_X,tr_Y)
+w = cal_weight(tr_X,tr_Y) #transposes into learned weight vector (creates model)
+
+print('1.1 Weight with dummy:')
+print(numpy.matrix(w)) #answer to 1.1
+
+#print(tr_X)
+
+#question 1.2
+print('1.2 ASE with dummy:')
+print(ASE(tr_X, tr_Y,w)) #prints ASE of training data
+print(ASE(ts_X, ts_Y,w)) #prints ASE of testing data
+
+#question 1.3
+tr_X, tr_Y = open_file(file_name1,0) #transposes training data
+ts_X, ts_Y = open_file(file_name2,0) #transposes testing data
+
+w = cal_weight(tr_X,tr_Y) #transposes into learned weight vector (creates model)
 print('Weight without dummy:')
 #print(tr_X)
-print(numpy.matrix(w))
+print(numpy.matrix(w)) ##probably don't need this line anymore?
+
 print('ASE without dummy:') 
-print(ASE(tr_X, tr_Y,w))
-print(ASE(ts_X, ts_Y,w))
+print(ASE(tr_X, tr_Y,w)) #prints ASE of training data
+print(ASE(ts_X, ts_Y,w)) #prints ASE of testing data
+
+#question 1.4
